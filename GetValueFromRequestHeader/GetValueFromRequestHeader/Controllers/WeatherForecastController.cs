d29c155b25a06e98718287433383141e74a68436
GadHao <524472212@qq.com>
2021-08-05 15:09:18 +0800

    feat: sample project for issue headers.TryGetValue

---
+++ GetValueFromRequestHeader/GetValueFromRequestHeader/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetValueFromRequestHeader.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ GetValueFromRequestHeader/GetValueFromRequestHeader/SampleFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GetValueFromRequestHeader
+{
+    public class SampleFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var headers = context.HttpContext.Request.Headers;
+            headers.TryGetValue("one", out var one);
+            headers.TryGetValue("two,", out var two);
+            headers.TryGetValue("three,", out var three);
+
+            System.Diagnostics.Debug.WriteLine($"header one :{one}");
+            System.Diagnostics.Debug.WriteLine($"header two :{two}");
+            System.Diagnostics.Debug.WriteLine($"header three :{three}");
+            System.Diagnostics.Debug.WriteLine($"header one :{headers["one"]}");
+            System.Diagnostics.Debug.WriteLine($"header two :{headers["two"]}");
+            System.Diagnostics.Debug.WriteLine($"header three :{headers["three"]}");
+        }
+    }
+}
