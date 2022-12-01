80219528800d877adfd80a7c0f6ec7ab5cdfeb04
KamenRiderKuuga <524472212@qq.com>
2022-12-01 18:23:25 +0800

    feat: 新增用于复现FreeSQL下划线模糊查询问题的项目

---
+++ FreeSqlLikeProblem/FreeSqlLikeProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿using FreeSql;
+using FreeSqlLikeProblem;
+using MySql.Data.MySqlClient;
+using System;
+
+var mysqlConnection = "Server=127.0.0.1;port=3306;Database=test;user id=root;password=123456;";
+
+using (var connection = new MySqlConnection(mysqlConnection))
+{
+    try
+    {
+        connection.Open();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"mysql连接错误--{ex.Message}");
+    }
+}
+
+var fsql = new FreeSqlBuilder().UseConnectionString(DataType.MySql, mysqlConnection).Build();
+var searchTerms = "姓名_";
+var names = fsql.Select<User>().Where(e => e.Name.Contains(searchTerms)).ToList(e => e.Name);
+Console.WriteLine($"关键词: {searchTerms}, 搜索结果：{string.Join(',', names)}");
+
+searchTerms = "姓名%";
+names = fsql.Select<User>().Where(e => e.Name.Contains(searchTerms)).ToList(e => e.Name);
+Console.WriteLine($"关键词: {searchTerms}, 搜索结果：{string.Join(',', names)}");
+
+
+searchTerms = "姓_";
+names = fsql.Select<User>().Where(e => e.Name.Contains(searchTerms)).ToList(e => e.Name);
+Console.WriteLine($"关键词: {searchTerms}, 搜索结果：{string.Join(',', names)}");
+
+
+searchTerms = "姓%";
+names = fsql.Select<User>().Where(e => e.Name.Contains(searchTerms)).ToList(e => e.Name);
+Console.WriteLine($"关键词: {searchTerms}, 搜索结果：{string.Join(',', names)}");
---
+++ FreeSqlLikeProblem/FreeSqlLikeProblem/ScriptToInitData.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE IF NOT EXISTS `tuser`  (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+replace into tuser(`Id`, `Name`, `Password`) values(1, '姓名1', 'mima1');
+replace into tuser(`Id`, `Name`, `Password`) values(2, '姓名2', 'mima2');
+replace into tuser(`Id`, `Name`, `Password`) values(3, '姓名3', 'mima3');
